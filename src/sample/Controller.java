7e9dcc8f0ba7cf96346465d7a3e575fc06c6712e
marimagalhaesl <maari.magalhaes@hotmail.com>
2021-04-09 15:54:09 -0700

    Step03

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,18 +3,18 @@ package sample;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ListView;
-import javafx.scene.control.RadioButton;
+import javafx.scene.control.*;
 import javafx.collections.FXCollections;
 import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Controller implements Initializable {
 
@@ -32,12 +32,11 @@ public class Controller implements Initializable {
     @FXML private ListView listView;
 
     //auxiliary variables
-    private ObservableList<String> airline = FXCollections.observableArrayList("Air Canada","Air India");
-    private ObservableList<String> airlineNum = FXCollections.observableArrayList("AC102","AC104","AC107","AC113","AI011");
-    private ObservableList<String> departureAirport = FXCollections.observableArrayList("YVR","YYZ");
-    private ObservableList<String> arrivalAirport = FXCollections.observableArrayList("YYZ", "YVR", "NRT");
+    private ObservableList<String> cBItem;
 
     public void HandleAirlineRadioBtn(){
+
+        airlineChoiceBox.getItems().clear();
 
         airlineNumRadioBtn.setSelected(false);
         departureRadioBtn.setSelected(false);
@@ -47,9 +46,36 @@ public class Controller implements Initializable {
         airlineNumChoiceBox.setDisable(true);
         departureChoiceBox.setDisable(true);
         arrivalChoiceBox.setDisable(true);
+
+        try {
+            File myObj = new File("/Users/marianamagalhaes/Desktop/assignment04/src/sample/Flight.txt");
+            Scanner myReader = new Scanner(myObj);
+            ArrayList<String> aL = new ArrayList<>();
+
+
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String[] token = data.split(",");
+                aL.add(token[0]);
+            }
+
+            List<String> aList = aL.stream().distinct().collect(Collectors.toList());
+            cBItem = FXCollections.observableArrayList(aList);
+
+            airlineChoiceBox.getItems().addAll(cBItem);
+
+            myReader.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
     }
 
     public void HandleAirlineNumRadioBtn(){
+
+        airlineNumChoiceBox.getItems().clear();
 
         airlineRadioBtn.setSelected(false);
         departureRadioBtn.setSelected(false);
@@ -59,9 +85,35 @@ public class Controller implements Initializable {
         airlineNumChoiceBox.setDisable(false);
         departureChoiceBox.setDisable(true);
         arrivalChoiceBox.setDisable(true);
+
+        try {
+            File myObj = new File("/Users/marianamagalhaes/Desktop/assignment04/src/sample/Flight.txt");
+            Scanner myReader = new Scanner(myObj);
+            ArrayList<String> aL = new ArrayList<>();
+
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String[] token = data.split(",");
+                aL.add(token[1]);
+            }
+
+            List<String> aList = aL.stream().distinct().collect(Collectors.toList());
+            cBItem = FXCollections.observableArrayList(aList);
+
+            airlineNumChoiceBox.getItems().addAll(cBItem);
+
+            myReader.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
     }
 
     public void HandleDepartureRadioBtn(){
+
+        departureChoiceBox.getItems().clear();
 
         airlineRadioBtn.setSelected(false);
         airlineNumRadioBtn.setSelected(false);
@@ -71,9 +123,35 @@ public class Controller implements Initializable {
         airlineNumChoiceBox.setDisable(true);
         departureChoiceBox.setDisable(false);
         arrivalChoiceBox.setDisable(true);
+
+        try {
+            File myObj = new File("/Users/marianamagalhaes/Desktop/assignment04/src/sample/Flight.txt");
+            Scanner myReader = new Scanner(myObj);
+            ArrayList<String> aL = new ArrayList<>();
+
+
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String[] token = data.split(",");
+                aL.add(token[2]);
+            }
+
+            List<String> aList = aL.stream().distinct().collect(Collectors.toList());
+            cBItem = FXCollections.observableArrayList(aList);
+
+            departureChoiceBox.getItems().addAll(cBItem);
+
+            myReader.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 
     public void HandleArrivalRadioBtn(){
+
+        arrivalChoiceBox.getItems().clear();
 
         airlineRadioBtn.setSelected(false);
         airlineNumRadioBtn.setSelected(false);
@@ -82,26 +160,33 @@ public class Controller implements Initializable {
         airlineChoiceBox.setDisable(true);
         airlineNumChoiceBox.setDisable(true);
         departureChoiceBox.setDisable(true);
-        arrivalChoiceBox.setDisable(false);    }
+        arrivalChoiceBox.setDisable(false);
 
-    public void HandleAirlineChoiceBox(){
-        System.out.println("Airline Choice Box");
+        try {
+            File myObj = new File("/Users/marianamagalhaes/Desktop/assignment04/src/sample/Flight.txt");
+            Scanner myReader = new Scanner(myObj);
+            ArrayList<String> aL = new ArrayList<>();
+
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String[] token = data.split(",");
+                aL.add(token[3]);
+            }
+
+            List<String> aList = aL.stream().distinct().collect(Collectors.toList());
+            cBItem = FXCollections.observableArrayList(aList);
+
+            arrivalChoiceBox.getItems().addAll(cBItem);
+
+            myReader.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 
-    public void HandleAirlineNumChoiceBox(){
-        System.out.println("Airline Number Choice Box");
-    }
-
-    public void HandleDepartureChoiceBox(){
-        System.out.println("Departure Choice Box");
-    }
-
-
-   //private String aC;
-
-    public void HandleArrivalChoiceBox(){
-      //System.out.println(arrivalChoiceBox.getSelectionModel().getSelectedItem());
-    }
+    private String choice;
 
     public void HandleSearchBtn(){
         listView.getItems().clear();
@@ -113,20 +198,11 @@ public class Controller implements Initializable {
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
                 String[] token = data.split(",");
-                /*System.out.println(token[0]);
-                System.out.println(token[1]);
-                System.out.println(token[2]);
-                System.out.println(token[3]);
-                System.out.println();*/
-                listView.getItems().add("\t" + token[0] + "\t\t\t\t" + token[1] + "\t\t\t\t" + token[2] + "\t\t\t\t\t" + token[3]);
-
+                if (choice.equals(token[0]) || choice.equals(token[1]) || choice.equals(token[2]) || choice.equals(token[3])) {
+                    listView.getItems().add("\t" + token[0] + "\t\t\t\t" + token[1] + "\t\t\t\t" + token[2] + "\t\t\t\t\t" + token[3]);
+                }
             }
             myReader.close();
-
-            System.out.println(airlineChoiceBox.getSelectionModel().getSelectedItem());
-            System.out.println(airlineNumChoiceBox.getSelectionModel().getSelectedItem());
-            System.out.println(departureChoiceBox.getSelectionModel().getSelectedItem());
-            System.out.println(arrivalChoiceBox.getSelectionModel().getSelectedItem());
 
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
@@ -139,26 +215,27 @@ public class Controller implements Initializable {
         stage.close();
     }
 
-    public void HandleListView(){
-
-    }
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        airlineChoiceBox.getItems().addAll(airline);
+
         airlineChoiceBox.setValue("Select Airline");
 
-        airlineNumChoiceBox.getItems().addAll(airlineNum);
+
         airlineNumChoiceBox.setValue("Select Airline Number");
 
-        departureChoiceBox.getItems().addAll(departureAirport);
+
         departureChoiceBox.setValue("Select Departure Airport");
 
-        arrivalChoiceBox.getItems().addAll(arrivalAirport);
+
         arrivalChoiceBox.setValue("Select Arrival Airport");
 
         listView.getItems().add("\t Airline \t\t\t\t AirlineNo \t\t\t\t Departure \t\t\t\t Arrival");
+
+        airlineChoiceBox.getSelectionModel().selectedItemProperty().addListener((v, ov, nv) -> choice = new String(nv.toString()));
+        airlineNumChoiceBox.getSelectionModel().selectedItemProperty().addListener((v, ov, nv) -> choice = new String(nv.toString()));
+        departureChoiceBox.getSelectionModel().selectedItemProperty().addListener((v, ov, nv) -> choice = new String(nv.toString()));
+        arrivalChoiceBox.getSelectionModel().selectedItemProperty().addListener((v, ov, nv) -> choice = new String(nv.toString()));
 
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Airline Information System");
-        primaryStage.setScene(new Scene(root, 810, 500));
+        primaryStage.setScene(new Scene(root, 804, 477));
         primaryStage.show();
     }
 
